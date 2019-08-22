b02b7a77cae69e26e571090d82c1f462e928e127
Zinzidane <ya-gans@mail.ru>
2019-08-22 21:02:18 +0300

    Добавляет вторичные ключи

--- schema.sql
+++ schema.sql
@@ -9,6 +9,16 @@ CREATE TABLE category (
   name VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE user (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  date_add DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  email VARCHAR(128) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  password VARCHAR(64) NOT NULL,
+  avatar VARCHAR(255),
+  contact TEXT
+);
+
 CREATE TABLE lot (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   category_id INT UNSIGNED NOT NULL,
@@ -20,7 +30,10 @@ CREATE TABLE lot (
   description VARCHAR(255),
   image VARCHAR(255),
   initial_rate INT UNSIGNED NOT NULL,
-  rate_step INT UNSIGNED NOT NULL
+  rate_step INT UNSIGNED NOT NULL,
+  CONSTRAINT fk_lot_category_id FOREIGN KEY (category_id) REFERENCES category (id),
+  CONSTRAINT fk_lot_user_id FOREIGN KEY (user_id) REFERENCES USER (id),
+  CONSTRAINT fk_lot_winner_id FOREIGN KEY (winner_id) REFERENCES USER (id)
 );
 
 CREATE TABLE bet (
@@ -28,20 +41,11 @@ CREATE TABLE bet (
   lot_id INT UNSIGNED NOT NULL,
   user_id INT UNSIGNED NOT NULL,
   date_add DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  rate INT UNSIGNED NOT NULL
-);
-
-CREATE TABLE user (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  date_add DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  email VARCHAR(128) NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  password VARCHAR(64) NOT NULL,
-  avatar VARCHAR(255),
-  contact TEXT
+  rate INT UNSIGNED NOT NULL,
+  CONSTRAINT fk_bet_lot_id FOREIGN KEY (lot_id) REFERENCES lot (id),
+  CONSTRAINT fk_bet_user_id FOREIGN KEY (user_id) REFERENCES USER (id)
 );
 
 CREATE INDEX title ON lot(title);
 CREATE INDEX description ON lot(description);
-CREATE UNIQUE INDEX lot_id ON lot(id);
 CREATE UNIQUE INDEX email ON user(email);
