c6ff57ae12c8c55e7d6e57ea63f3668884fef41c
Zinzidane <ya-gans@mail.ru>
2019-08-24 14:06:34 +0300

    Добавляет страницу с лотом

---
+++ lot.php
@@ -0,0 +1,51 @@
+<?php
+require_once('init.php');
+
+if (!$link) {
+    $error = mysqli_connect_error();
+    print('Проблема с базой данных. Ожидайте пока исправим.');
+    exit;
+}
+
+if (isset($_GET['id'])) {
+    $lot_id = $_GET['id'];
+    $categories_sql = 'SELECT name, symbol_code FROM category';
+    $lot_sql = 'SELECT lot.title, lot.initial_rate, lot.rate_step, lot.image, lot.date_close, MAX(bet.rate) AS current_rate, category.name AS category, COUNT(bet.lot_id) AS bets_number FROM lot '
+    . 'JOIN category ON lot.category_id = category.id '
+    . 'JOIN bet ON lot.id = bet.lot_id '
+    . 'WHERE lot.id = ' . $lot_id;
+    $bets_sql = 'SELECT bet.rate as rate, bet.date_add as date_add, user.name as user FROM lot '
+    . 'JOIN bet ON lot.id = bet.lot_id '
+    . 'JOIN user on user.id = bet.user_id '
+    . 'WHERE lot.id = ' . $lot_id
+    . ' ORDER BY lot.date_add DESC';
+
+
+    $categories_result = mysqli_query($link, $categories_sql);
+    $lot_result = mysqli_query($link, $lot_sql);
+    $bets_result = mysqli_query($link, $bets_sql);
+
+    if (!$categories_result || !$lot_result || !$bets_result) {
+        $error = mysqli_error($link);
+        header("HTTP/1.0 404 Not Found");
+    }
+    $is_auth = rand(0, 1);
+    $categories = mysqli_fetch_all($categories_result, MYSQLI_ASSOC);
+    $lot = mysqli_fetch_all($lot_result, MYSQLI_ASSOC)[0];
+    $bets = mysqli_fetch_all($bets_result, MYSQLI_ASSOC);
+}
+else {
+    header("HTTP/1.0 404 Not Found");
+}
+
+$page_content = include_template('lot.php', ['lot' => $lot, 'bets' => $bets, 'is_auth' => $is_auth]);
+$layout_content = include_template('layout.php', [
+    'title' => 'Главная',
+    'username' => 'Ваня',
+    'is_auth' => $is_auth,
+    'content' => $page_content,
+    'categories' => $categories
+]);
+
+
+print($layout_content);
---
+++ templates/lot.php
@@ -0,0 +1,56 @@
+<section class="lot-item container">
+    <h2><?=$lot['title']; ?></h2>
+    <div class="lot-item__content">
+    <div class="lot-item__left">
+        <div class="lot-item__image">
+        <img src="../<?=$lot['image']; ?>" width="730" height="548" alt=<?=$lot['title']; ?>>
+        </div>
+        <p class="lot-item__category">Категория: <span><?=$lot['category']; ?></span></p>
+        <p class="lot-item__description"><?=$lot['description']; ?></p>
+    </div>
+    <div class="lot-item__right">
+        <div class="lot-item__state">
+        <?php if (get_dt_range($lot['date_close'])[0] < 1): ?>
+        <div class="lot__timer timer--finishing">
+            <?=implode(':', get_dt_range($lot['date_close']));?>
+        </div>
+        <?php else: ?>
+        <div class="lot__timer">
+            <?=implode(':', get_dt_range($lot['date_close']));?>
+        </div>
+        <?php endif;?>
+        <div class="lot-item__cost-state">
+            <div class="lot-item__rate">
+            <span class="lot-item__amount">Текущая цена</span>
+            <span class="lot-item__cost"><?=add_currency_to_price(format_price(calculate_current_price($lot['initial_rate'], $lot['current_rate'])), 'rub', 'р'); ?></span>
+            </div>
+            <div class="lot-item__min-cost">
+            Мин. ставка <span><?=add_currency_to_price(calculate_minimal_bet($lot['initial_rate'], $lot['rate_step'], $lot['current_rate']), 'rub', 'р'); ?></span>
+            </div>
+        </div>
+        <?php if ($is_auth): ?>
+        <form class="lot-item__form" action="https://echo.htmlacademy.ru" method="post" autocomplete="off">
+            <p class="lot-item__form-item form__item form__item--invalid">
+            <label for="cost">Ваша ставка</label>
+            <input id="cost" type="text" name="cost" placeholder="<?=calculate_minimal_bet($lot['initial_rate'], $lot['rate_step'], $lot['current_rate']); ?>">
+            <span class="form__error">Введите наименование лота</span>
+            </p>
+            <button type="submit" class="button">Сделать ставку</button>
+        </form>
+        <?php endif;?>
+        </div>
+        <div class="history">
+        <h3>История ставок (<span><?=$lot['bets_number']; ?></span>)</h3>
+        <table class="history__list">
+            <?php foreach ($bets as $bet): ?>
+            <tr class="history__item">
+            <td class="history__name"><?=$bet['user']; ?></td>
+            <td class="history__price"><?=add_currency_to_price(format_price(htmlspecialchars($bet['rate'])), 'rub', 'р'); ?></td>
+            <td class="history__time"><?=$bet['date_add']; ?></td>
+            </tr>
+            <?php endforeach; ?>
+        </table>
+        </div>
+    </div>
+    </div>
+</section>
