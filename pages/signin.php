c84e7a0a143384ecc65b35e3f01356ace8720e30
imuliarchyk <imuliarchyk@orion.intra>
2019-09-04 16:19:39 +0300

    Убирает заккоментированный блок

--- pages/signin.php
+++ pages/signin.php
@@ -23,15 +23,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $errors['email'] = 'Такой пользователь не найден';
             $page_content = include_template('signin.php', ['signin_form' => $signin_form, 'errors' => $errors]);
         } else {
-            // var_dump(password_verify($signin_form['password'], $user[0]['password']), $user);
             if (password_verify($signin_form['password'], $user['password'])) {
                 $_SESSION['user'] = $user;
             } else {
                 $errors['password'] = 'Неверный пароль';
             }
-            // $user_add_sql = 'INSERT INTO user (email, name, password) VALUES (?, ?, ?)';
-            // $user_add_stmt = db_get_prepare_stmt($link, $user_add_sql, [$signin_form['email'], $signin_form['name'], $password]);
-            // $user_add_res = mysqli_stmt_execute($user_add_stmt);
         }
 
         if ($user && empty($errors)) {
